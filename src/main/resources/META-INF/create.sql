28a377d8f95f942c4809f8141168d48fa333ae7d
Ariel Rosales <arosalesfarias@gmail.com>
2019-04-26 16:56:25 -0300

    Parte 2 ejercicio 2 hecho

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -11,5 +11,26 @@ from joyts.Pelicula
 where f.id = sala and proy.genero = "Accion";
 END//
 DELIMITER ;
+--2
+create table if not exists joyts.pelicula_historico(
+proyeccion_id bigint(20),
+fecha_modificacion datetime,
+titulo_anterior varchar(50),
+titulo_nuevo varchar(50),
+primary key(proyeccion_id,fecha_modificacion),
+foreign key fk_proy(proyeccion_id)
+references joyts.pelicula(id)
+);
+
+delimiter |
+CREATE TRIGGER historico_peliculas BEFORE UPDATE ON joyts.pelicula
+  FOR EACH ROW 
+  BEGIN
+  declare anterior varchar(50);
+  select titulo from joyts.pelicula where id = new.id into anterior;
+    INSERT INTO joyts.pelicula_historico SET proyeccion_id = NEW.id, fecha_modificacion = now(), titulo_anterior = anterior, titulo_nuevo = new.titulo;
+  END;
+  |
+delimiter ;
 --4
 ALTER TABLE joyts.usuario ALTER saldo SET DEFAULT 0;
