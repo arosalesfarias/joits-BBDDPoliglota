954c69f319d925632e9fac73b7e01825254eefbe
Ariel Rosales <arosalesfarias@gmail.com>
2019-04-30 17:10:29 -0300

    Se modifico store procedure debido a la union de peliculas y sagas en una sola tabla(proyeccion)

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -3,12 +3,8 @@ DELIMITER //
 CREATE PROCEDURE joyts.salasDeAccionFinal(in sala int)
 begin select titulo,añoRodaje,puntaje from joyts.funcion f
 inner join joyts.proyeccion_funcion pf on pf.funciones_id = f.id
-inner join (select id,titulo,añoRodaje,genero,puntaje 
-from joyts.Pelicula 
-	union
-    all select id,titulo,añoRodaje,genero,puntaje 
-            from joyts.Saga) proy on proy.id = pf.proyeccion_id
-where f.id = sala and proy.genero = "Accion";
+inner join joyts.proyeccion proy on proy.id = pf.proyeccion_id
+where f.sala = sala and proy.genero = "Accion";
 END//
 DELIMITER ;
 --2
